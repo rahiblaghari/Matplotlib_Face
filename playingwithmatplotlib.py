84b3905ff72b34b901c78c6cea3c1e408f393e2e
rahiblaghari <53871954+rahiblaghari@users.noreply.github.com>
2020-07-05 17:10:55 -0500

    Add files via upload

---
+++ playingwithmatplotlib.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as lt
+lt.rcParams['axes.facecolor'] = 'grey'
+lt.figure().patch.set_facecolor('xkcd:purple')
+x=[1,1,5,5]
+y=[2,1.8,1.8,2]
+lt.plot(x,y)
+lt.fill_between([1,1.2,1.2,1,1],[3,3,2.75,2.75,3])
+lt.fill_between([4.8,5,5,4.8,4.8],[3,3,2.75,2.75,3])
+lt.fill_between([3,2.5,3.5,3],[2.6,2.4,2.4,2.6])
+lt.xlabel("x")
+lt.ylabel("y")
+lt.title("Title")
+lt.legend()
+lt.show()
